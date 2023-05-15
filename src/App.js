6a9214906286cf18685ee6c5069b745b3014891d
mayramartins <mayramartins.in@gmail.com>
2023-05-15 18:15:13 -0300

    wip: lat and long data implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      console.log("Latitude is:", position.coords.latitude);
+      console.log("Longitude is:", position.coords.longitude);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Weather Wonder App</h1>
+    </div>
+  );
 }
 
 export default App;
