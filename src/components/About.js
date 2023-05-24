de995b36b7ab1ceaf5b74f30a0aa3e56573407f4
mayramartins <mayramartins.in@gmail.com>
2023-05-24 12:24:37 -0300

    refining project

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,8 @@ const About = () => {
     <div style={{ textAlign: "center" }}>
       <p>Weather Wonder 2023</p>
       <p>
-        Created with ❤️ by{" "}
-        <a href="https://github.com/mayramartins?tab=repositories">
-          Mayra Martins{" "}
-        </a>
-        to my grandmother Imaculada Maria Pinto
+        {" "}
+        Created with ❤️ by Mayra Martins to my grandmother Imaculada Maria Pinto
       </p>
     </div>
   );
