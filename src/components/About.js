d54c2733bfa1c836365af05d0d829fce46662cfa
mayramartins <mayramartins.in@gmail.com>
2023-05-24 12:51:25 -0300

    las commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,6 @@ const About = () => {
     <div style={{ textAlign: "center" }}>
       <p>Weather Wonder 2023</p>
       <p>
-        {" "}
         Created with ❤️ by Mayra Martins to my grandmother Imaculada Maria Pinto
       </p>
     </div>
